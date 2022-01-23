4873b0602314278b56e728a9386373e995801df9
Leonel Binique <leonel.binique@gmail.com>
2022-01-23 15:19:14 +0100

    Création class Question

---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  late String listQuestions;
+  late String button;
+  late String button1;
+
+  Question(String q, String b, String b1){
+    listQuestions = q;
+    button = b;
+    button1 = b1;
+
+  }
+
+}
